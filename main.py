d9aeaceffbd4b43145f7b159943ed75bcdd030e7
Cam Sima <cjsima@gmail.com>
2023-04-01 13:56:34 -0400

    add healthcheck

--- main.py
+++ main.py
@@ -31,3 +31,8 @@ def get_search_video(video_id: str, text: str, request: Request):
         "result": result,
         "helpers": helpers
     })
+
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
