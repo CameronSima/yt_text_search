c85f0ee7553cc77773ef2bad9999673e700b9f0b
Cam Sima <cjsima@gmail.com>
2023-04-02 11:15:24 -0400

    updated pubsub

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.util import get_remote_address
 from service.search import search_video
 from service.logger import log
+from service.pubsub import parse_notification
 import template_helpers as helpers
 
 limiter = Limiter(key_func=get_remote_address)
@@ -38,9 +39,12 @@ def get_search_video(video_id: str, text: str, request: Request):
     return render("video_results.jinja2", context)
 
 
-@app.get("/api/yt_sub")
-def sub_callback(request: Request):
-    log(f"Received sub callback {request}")
+@app.post("/api/yt_sub")
+async def sub_callback(request: Request):
+    data = await request.json()
+    log(f"Received sub callback {data}")
+    parsed = parse_notification(data)
+    log(f"Parsed sub callback {parsed}")
     return {"status": "ok"}
 
 
--- service/pubsub.py
+++ service/pubsub.py
@@ -1,16 +1,9 @@
 import requests
+import xml.etree.ElementTree as ET
 
 URL = "https://pubsubhubbub.appspot.com/subscribe"
 TOPIC_BASE_URL = "https://www.youtube.com/xml/feeds/videos.xml?channel_id="
 CALLBACK_URL = "https://yttextsearch-production.up.railway.app/api/yt_sub"
-
-# hub.callback: https://ergsd.com/hook
-# hub.topic: https://www.youtube.com/xml/feeds/videos.xml?channel_id=thehistorysquad
-# hub.verify: async
-# hub.mode: subscribe
-# hub.verify_token:
-# hub.secret:
-# hub.lease_numbers:
 
 
 def subscribe(channel_id: str):
@@ -29,6 +22,16 @@ def subscribe(channel_id: str):
     response = requests.post(URL, data=data, headers=headers)
     print(response.status_code)
     print(response.text)
+
+
+def parse_notification(data: str):
+    root = ET.fromstring(data)
+    entry = root.find('{http://www.w3.org/2005/Atom}entry')
+    video_id = entry.find(
+        '{http://www.youtube.com/xml/schemas/2015}videoId').text
+    return {
+        "video_id": video_id
+    }
 
 
 if __name__ == '__main__':
