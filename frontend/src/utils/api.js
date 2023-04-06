9c8a4bf628e12e5005fb3a01c31933fcd9c3fbb9
Cam Sima <cjsima@gmail.com>
2023-04-06 19:21:04 -0400

    ...

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,4 +1,7 @@
-export const API_URL = "http://localhost:8000/api/";
+export const API_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://yttextsearch-production.up.railway.app/"
+    : "http://localhost:8000/api/";
 
 export function post(path, data) {
   return fetch(API_URL + path, {
