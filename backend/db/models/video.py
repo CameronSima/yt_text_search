0504efc941d45c34be5899ee9c0757ede87626e6
Cameron Sima <cjsima@gmail.com>
2023-04-12 09:27:27 -0400

    Update video.py
    add db index

--- backend/db/models/video.py
+++ backend/db/models/video.py
@@ -6,7 +6,7 @@ class Video(Model):
     id = fields.IntField(pk=True)
     yt_video_id = fields.CharField(max_length=255)
     yt_channel_id = fields.CharField(max_length=255)
-    title = fields.CharField(max_length=255)
+    title = fields.CharField(max_length=255, index=True)
     url = fields.CharField(max_length=255)
     published_at = fields.DatetimeField(auto_now_add=True)
 
