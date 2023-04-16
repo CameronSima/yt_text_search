2a0433d09f48086fb3490109369cc17aa04a687f
Cam Sima <cjsima@gmail.com>
2023-04-16 12:35:17 -0400

    ...

--- backend/service/youtube_api.py
+++ backend/service/youtube_api.py
@@ -63,13 +63,9 @@ async def get_channel_id(username):
         print('External ID not found')
 
 
-async def get_playlist_id_from_channel_id(channel_id) -> str:
-    url = f"https://www.googleapis.com/youtube/v3/channels?part=contentDetails&id={channel_id}&key={YOUTUBE_API_KEY}"
-    response = await http_client.get(url)
-    data_json = response.json()
-    print(data_json)
-    data = data_json['items']
-    return data[0]['contentDetails']['relatedPlaylists']['uploads']
+def get_playlist_id_from_channel_id(channel_id: str) -> str:
+    """Replace UC with UU to get the uploads playlist id"""
+    return channel_id.replace('UC', 'UU')
 
 
 async def get_playlist_id_from_channel_name(channel_name: str) -> list[dict]:
@@ -89,23 +85,34 @@ async def get_videos_from_playlist_id(playlists_id: str, page_token: str or None
 
     response = await http_client.get(url)
     data_json = response.json()
-    print(data_json)
     data = data_json['items']
-    videos = []
-    for video in data:
-        videos.append(
-            Video(
-                id=video['id']['videoId'],
-                title=video['snippet']['title'],
-                description=video['snippet']['description'],
-                published_at=video['snippet']['publishedAt'],
-                channel_id=video['snippet']['channelId'],
-            ))
+
+    def map_video(video) -> Video:
+        return Video(
+            id=video['snippet']['resourceId']['videoId'],
+            title=video['snippet']['title'],
+            description=video['snippet']['description'],
+            published_at=video['snippet']['publishedAt'],
+            channel_id=video['snippet']['channelId'],
+        )
+
+    videos = [map_video(video) for video in data]
+
+    # videos = []
+    # for video in data:
+    #     videos.append(
+    #         Video(
+    #             id=video['id'],
+    #             title=video['snippet']['title'],
+    #             description=video['snippet']['description'],
+    #             published_at=video['snippet']['publishedAt'],
+    #             channel_id=video['snippet']['channelId'],
+    #         ))
 
     return {
         'videos': videos,
         'pagedResults': len(videos),
-        'totalResults': data_json['pageInfo']['totalResults'],
+        'totalResults': data_json.get('pageInfo', {}).get('totalResults', 0),
         'nextPageToken': data_json.get('nextPageToken', None),
     }
 
@@ -136,7 +143,8 @@ class ChannelVideos:
         self.next_page_token = None
 
     async def init(self):
-        self.channel_playlist_id = await get_playlist_id_from_channel_id(self.channel_id)
+        self.channel_playlist_id = get_playlist_id_from_channel_id(
+            self.channel_id)
         self.videos = await self.get_channel_videos()
 
     async def videos_generator(self):
@@ -150,7 +158,7 @@ class ChannelVideos:
         self.videos = new_vids
 
     async def get_channel_videos(self, page_token=None) -> tuple[list[Video], str]:
-        results = await get_videos_from_playlist_id(self.channel_id, page_token)
+        results = await get_videos_from_playlist_id(self.channel_playlist_id, page_token)
         self.next_page_token = results.get('nextPageToken')
         self.total_channel_videos = results.get('totalResults')
         return results.get('videos')
